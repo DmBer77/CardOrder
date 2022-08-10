b0cbe95073a8015794fc6978699b6df68a0901f8
Dmitry Berezovsky <dm.berezovsky@gmail.com>
2022-08-10 15:15:40 +0300

    prepare for writing test

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -1,3 +1,4 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -5,10 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
 public class CardOrderTest {
-
 
     WebDriver driver;
 
@@ -28,8 +26,15 @@ public class CardOrderTest {
     }
 
     @Test
-    void test() {
-        // Your test logic here
+    void shouldSubmitRequest() throws InterruptedException {
+        driver.get("http://localhost:9999");
+//        WebElement form = driver.findElement(By.cssSelector("[.form form_size_m form_theme_alfa-on-white]"));
+//        form.findElement(By.cssSelector("[data-test-id=name] input__top")).sendKeys("Иванов Петр");
+//        form.findElement(By.cssSelector("[data-test-id=phone] input__top")).sendKeys("+79101234567");
+//        form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+//        form.findElement(By.cssSelector("[data-test-id=submit]")).click();
+//        String text = driver.findElement(By.className("order-success")).getText();
+//        assertEquals("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 
 }
