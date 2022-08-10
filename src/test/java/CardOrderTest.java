ddb7fbf04788293ec5dba25a098acfe78092559b
Dmitry Berezovsky <dm.berezovsky@gmail.com>
2022-08-10 17:25:24 +0300

    local test passed

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -3,8 +3,12 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CardOrderTest {
 
@@ -17,7 +21,11 @@ public class CardOrderTest {
 
     @BeforeEach
     void setup() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
@@ -26,19 +34,28 @@ public class CardOrderTest {
     }
 
     @Test
-    void shouldSubmitRequest() throws InterruptedException {
+    void shouldSubmitRequestByFirstOption() {
         driver.get("http://localhost:9999");
-//        WebElement form = driver.findElement(By.cssSelector("[.form form_size_m form_theme_alfa-on-white]"));
-//        form.findElement(By.cssSelector("[data-test-id=name] input__top")).sendKeys("Иванов Петр");
-//        form.findElement(By.cssSelector("[data-test-id=phone] input__top")).sendKeys("+79101234567");
-//        form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-//        form.findElement(By.cssSelector("[data-test-id=submit]")).click();
-//        String text = driver.findElement(By.className("order-success")).getText();
-//        assertEquals("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+        driver.findElement(By.name("name")).sendKeys("Иванов Петр");
+        driver.findElement(By.name("phone")).sendKeys("+79101234567");
+        driver.findElement(By.className("checkbox")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+
+    @Test
+    void shouldSubmitRequestBySecondOption() throws InterruptedException {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Петр");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79101234567");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 
 }
-
 
 
 
