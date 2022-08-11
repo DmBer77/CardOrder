b69e1e13eba71e46a686a60028ecb3bf2b3fe1ee
Dmitry Berezovsky <dm.berezovsky@gmail.com>
2022-08-11 12:32:17 +0300

    task 2.1.2

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -5,10 +5,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class CardOrderTest {
 
@@ -55,6 +57,38 @@ public class CardOrderTest {
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 
+    @Test
+    void shouldCheckTheFieldsFillingVersionFirst() throws InterruptedException {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Петр1");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79101234567");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=name] .input__sub")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+    }
+
+    @Test
+    void shouldCheckTheFieldsFillingVersionSecond() throws InterruptedException {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Петр");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("79101234567");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=phone] .input__sub")).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
+    }
+
+    @Test
+    void shouldCheckTheFieldsFillingVersionThird() throws InterruptedException {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Петр");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79101234567");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]"));
+        driver.findElement(By.className("button")).click();
+        WebElement check = driver.findElement(By.className("checkbox"));
+        assertFalse(check.isSelected());
+    }
 }
 
 
