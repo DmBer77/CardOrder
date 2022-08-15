5fab3b205495b84073c12719143d643c9bb53d84
Dmitry Berezovsky <dm.berezovsky@gmail.com>
2022-08-15 11:41:06 +0300

    add new tests

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -47,46 +47,90 @@ public class CardOrderTest {
     }
 
     @Test
-    void shouldSubmitRequestBySecondOption() throws InterruptedException {
+    void shouldSubmitRequestBySecondOption() {
         driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Петр");
-        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79101234567");
-        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов Петр");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79101234567");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+
+    @Test
+    void shouldCheckTheFieldsFillingVersionFirstPointOne() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов Петр1");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79101234567");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+    }
+
+    @Test
+    void shouldCheckTheFieldsFillingVersionFirstPointTwo() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов-Задунайский Петр");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79101234567");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
         driver.findElement(By.className("button")).click();
         String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 
     @Test
-    void shouldCheckTheFieldsFillingVersionFirst() throws InterruptedException {
+    void shouldCheckTheFieldsFillingVersionFirstPointThree() {
         driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Петр1");
-        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79101234567");
-        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79101234567");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
         driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.cssSelector("[data-test-id=name] .input__sub")).getText();
-        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+        String text = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText();
+        assertEquals("Поле обязательно для заполнения", text.trim());
     }
 
     @Test
-    void shouldCheckTheFieldsFillingVersionSecond() throws InterruptedException {
+    void shouldCheckTheFieldsFillingVersionSecondPointOne() {
         driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Петр");
-        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("79101234567");
-        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов Петр");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("79101234567");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
         driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.cssSelector("[data-test-id=phone] .input__sub")).getText();
+        String text = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).getText();
         assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
     }
 
     @Test
-    void shouldCheckTheFieldsFillingVersionThird() throws InterruptedException {
+    void shouldCheckTheFieldsFillingVersionSecondPointTwo() {
         driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Петр");
-        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79101234567");
-        driver.findElement(By.cssSelector("[data-test-id=agreement]"));
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов Петр");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("a79101234567");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
         driver.findElement(By.className("button")).click();
-        WebElement check = driver.findElement(By.className("checkbox"));
+        String text = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
+    }
+
+    @Test
+    void shouldCheckTheFieldsFillingVersionSecondPointThree() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов Петр");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).getText();
+        assertEquals("Поле обязательно для заполнения", text.trim());
+    }
+
+    @Test
+    void shouldCheckTheFieldsFillingVersionThirdPointOne() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов Петр");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79101234567");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']"));
+        driver.findElement(By.className("button")).click();
+        WebElement check = driver.findElement(By.cssSelector("[data-test-id='agreement'].input_invalid .checkbox__control"));
         assertFalse(check.isSelected());
     }
 }
